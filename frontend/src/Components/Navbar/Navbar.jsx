ff701af1b067ca4ce98ce6eeac4ce51fb4cce8fe
SuryaRajendr <surya.crajendran@gmail.com>
2024-10-27 02:09:36 +0530

    E-commerce frontend react setup & Navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>Surya Shopper</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kidds</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
