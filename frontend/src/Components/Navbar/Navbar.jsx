cd2e2928633954e50538d44a4305b364d6c41a7b
SuryaRajendr <surya.crajendran@gmail.com>
2024-10-28 14:36:47 +0530

    Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("Shop")
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -11,14 +13,15 @@ const Navbar = () => {
             <p>Surya Shopper</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kidds</li>
+            <li onClick={() => {setMenu("Shop")}} >Shop {menu === "Shop"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Men")}} >Men {menu === "Men"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Women")}} >Women {menu === "Women"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Kidds")}} >Kidds {menu === "Kidds"?<hr></hr>:<></>} </li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart} alt="" />
+            <div className="nav_cart_count">0</div>
         </div>
     </div>
   )
