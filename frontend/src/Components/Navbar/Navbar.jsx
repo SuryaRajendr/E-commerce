2da5ec6c2b651bf19b96d980edd36df01d2f10ca
Surya Crajendran <73171944+SuryaRajendr@users.noreply.github.com>
2025-01-13 09:15:03 +0530

    Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -13,14 +14,14 @@ const Navbar = () => {
             <p>Surya Shopper</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={() => {setMenu("Shop")}} >Shop {menu === "Shop"?<hr></hr>:<></>} </li>
-            <li onClick={() => {setMenu("Men")}} >Men {menu === "Men"?<hr></hr>:<></>} </li>
-            <li onClick={() => {setMenu("Women")}} >Women {menu === "Women"?<hr></hr>:<></>} </li>
-            <li onClick={() => {setMenu("Kidds")}} >Kidds {menu === "Kidds"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Shop")}} > <Link to='/'>Shop</Link> {menu === "Shop"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Men")}} > <Link to='/mens'>Men</Link>  {menu === "Men"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Women")}} > <Link to='/womens'>Women</Link> {menu === "Women"?<hr></hr>:<></>} </li>
+            <li onClick={() => {setMenu("Kidds")}} > <Link to='/kids'>kids</Link> {menu === "Kidds"?<hr></hr>:<></>} </li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart} alt="" />
+            <Link to='/login'><button>Login</button> </Link>
+            <link to ='/cart'> < img src={cart} alt="" /> </link>
             <div className="nav_cart_count">0</div>
         </div>
     </div>
